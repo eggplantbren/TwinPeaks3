207afd20177bf64fb42529d33fbfdafefea47898
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-16 17:45:38 -0500

    More features, fixed ESS calc

--- Code/showresults.py
+++ Code/showresults.py
@@ -1,6 +1,7 @@
 from pylab import *
 from postprocess import logsumexp
 
+# First calculate things about the scalars (e.g. the normalising constant)
 scalars = loadtxt('scalars.txt')
 logw = loadtxt('logw.txt')
 smallest = min([scalars.shape[0], logw.size])
@@ -19,10 +20,31 @@ print('log(Z) = {logZ}'.format(logZ=logZ))
 
 # Posterior weights, normalised
 logWW = logW - logZ
-ess = -sum(exp(logWW)*logWW)
+ess = exp(-sum(exp(logWW)*logWW))
 
 plot(exp(logWW))
-ylabel('Posterior Weight')
-title('ESS = {ess}'.format(ess=ess))
+ylabel('Weight wrt canonical distribution')
+title('ESS (for purposes of normaliser) = {ess}'.format(ess=ess))
 show()
+
+
+## Now do it all again with the thinned samples
+#scalars = loadtxt('scalars_thinned.txt')
+#logw = loadtxt('logw_thinned.txt')
+#smallest = min([scalars.shape[0], logw.size])
+#scalars = scalars[0:smallest, :]
+#logw = logw[0:smallest]
+
+## Prior weights, normalised
+#logw = logw - logsumexp(logw)
+
+## Posterior weights, unnormalised
+#logW = logw + 10*scalars[:,0] + scalars[:,1]
+
+## Normaliser
+#logZ = logsumexp(logW)
+
+## Posterior weights, normalised
+#logWW = logW - logZ
+#ess = exp(-sum(exp(logWW)*logWW))
 
