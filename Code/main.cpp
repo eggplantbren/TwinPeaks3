0c169898a2b9e3cc635c9c8e026344facdb8bc1a
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-19 06:35:11 +1200

    Asked whether to append or overwrite files

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 #include <RandomNumberGenerator.h>
 #include "Sampler.h"
 #include "MyModel.h"
@@ -9,6 +10,22 @@ using namespace DNest3;
 
 int main()
 {
+	char choice;
+	do
+	{
+		cout<<"# [O]verwrite or [A]ppend output files? ";
+		cin>>choice;
+		choice = tolower(choice);
+	}while(choice != 'o' && choice != 'a');
+	if(choice == 'o')
+	{
+		int result = system("rm logw_thinned.txt logw.txt sample.txt scalars_thinned.txt scalars.txt");
+		if(result == 0)
+			cout<<"# Files removed."<<endl;
+		else
+			cout<<"# Files didn't exist."<<endl;
+	}
+
 	// Initialise RNG and seed with time
 	RandomNumberGenerator::initialise_instance();
 	RandomNumberGenerator::get_instance().set_seed(time(0));
