6b2c04f15184aab3f5c34505036764f42a40061d
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-07-23 15:53:25 +1200

    Do both old and new thing

--- Code/truth.py
+++ Code/truth.py
@@ -1,8 +1,24 @@
 from pylab import *
 
-def truth():
-  x = linspace(0., 1., 2001)
+x = linspace(0., 1., 2001)
 
+def truth(L1, L2, do_plot=False):
+  p = exp(-L1*(x - 0.5)**2 - L2*sin(2.*pi*x/0.5)**2)
+  Z = trapz(p, x=x)
+  H = trapz(p/Z*log(p/Z), x=x)
+
+  logZ = 200*log(Z)
+  H *= 200
+
+  if do_plot:
+    plot(x, p)
+    title([logZ, H])
+    show()
+
+  return [logZ, H]
+  
+
+def grid():
   # The two lagrange multipliers
   L1 = linspace(0., 20., 101)
   L2 = linspace(0., 20., 101)
@@ -14,12 +30,7 @@ def truth():
 
   for i in xrange(0, 101):
     for j in xrange(0, 101):
-      p = exp(-L1[i, j]*(x - 0.5)**2 - L2[i, j]*sin(2.*pi*x/0.5)**2)
-      Z = trapz(p, x=x)
-      H = trapz(p/Z*log(p/Z), x=x)
-
-      logZ[i, j] = 200*log(Z)
-      info[i, j] = H
+      [logZ[i, j], info[i, j]] = truth(L1[i, j], L2[i, j])
     print(i+1)
 
   subplot(1, 2, 1)
@@ -31,5 +42,7 @@ def truth():
 
 
 if __name__ == '__main__':
-  truth()
+  truth(10., 1., do_plot=True)
+  grid()
+
 
