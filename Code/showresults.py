eede6fd2d55410af61fd3c7772049a641bce42cf
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-09-04 13:58:22 +1200

    Show things as a function of temperature

--- Code/showresults.py
+++ Code/showresults.py
@@ -35,6 +35,56 @@ ylabel('Weight wrt canonical distribution')
 title('ESS (for purposes of normalising constant calc) = {ess}'.format(ess=ess))
 show()
 
+
+# Calculate log(Z) and H for some canonical distributions
+T1 = exp(linspace(-2., 10., 101))
+T2 = exp(linspace(-2., 10., 101))
+[T1, T2] = meshgrid(T1, T2)
+T2 = T2[::-1, :]
+logZ = T1.copy()
+H = T1.copy()
+ess = T1.copy()
+
+for i in xrange(0, 101):
+  for j in xrange(0, 101):
+    # Prior weights, normalised
+    logw = logw - logsumexp(logw)
+
+    # Posterior weights, unnormalised
+    logW = logw + scalars[:,0]/T1[i, j] + scalars[:,1]/T2[i, j]
+
+    # Normaliser
+    logZ[i, j] = logsumexp(logW)
+
+    # Posterior weights, normalised
+    logWW = logW - logZ[i, j]
+    ess[i,j] = exp(-sum(exp(logWW)*logWW))
+
+    # Information
+    H[i, j] = sum(exp(logWW)*(logWW - logw))
+  print(i+1)
+
+figure(1)
+subplot(1, 2, 1)
+imshow(logZ, extent=(log(T1.min()), log(T1.max()), log(T2.min()), log(T2.max())))
+xlabel('ln($T_1$)')
+ylabel('ln($T_2$)')
+title('log(Z)')
+
+subplot(1, 2, 2)
+imshow(H, extent=(log(T1.min()), log(T1.max()), log(T2.min()), log(T2.max())))
+xlabel('ln($T_1$)')
+ylabel('ln($T_2$)')
+title('H, max={m}'.format(m=H.max()))
+
+figure(2)
+imshow(ess, extent=(log(T1.min()), log(T1.max()), log(T2.min()), log(T2.max())))
+xlabel('ln($T_1$)')
+ylabel('ln($T_2$)')
+title('ESS, max = {m}'.format(m=ess.max()))
+
+show()
+
 ## Now do it all again with the thinned samples
 #scalars = loadtxt('scalars_thinned.txt')
 #logw = loadtxt('logw_thinned.txt')
