2b1966b2a4d8022a0cbc7cf8ec1243b47743f0a4
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-10-09 11:13:36 +1300

    For combining runs

---
+++ Code/Runs/gather.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	vector<string> directories;
+	directories.push_back(string("10.1"));
+	directories.push_back(string("10.2"));
+	directories.push_back(string("10.3"));
+	directories.push_back(string("10.4"));
+	directories.push_back(string("10.5"));
+	directories.push_back(string("10.6"));
+
+	fstream fout1("logw.txt", ios::out);
+	fstream fout2("scalars.txt", ios::out);
+
+	int k = 0;
+	for(size_t i=0; i<directories.size(); i++)
+	{
+		string filename1 = directories[i] + string("/logw.txt");
+		fstream fin1(filename1.c_str(), ios::in);
+
+		string filename2 = directories[i] + string("/scalars.txt");
+		fstream fin2(filename2.c_str(), ios::in);
+
+		double temp1, temp2, temp3;
+		while(fin1>>temp1 && fin2>>temp2 && fin2>>temp3)
+		{
+			fout1<<temp1<<endl;
+			fout2<<temp2<<' '<<temp3<<endl;
+			k++;
+		}
+
+		fin1.close();
+		fin2.close();
+
+		cout<<"# Processed "<<k<<" points."<<endl;
+	}
+
+	fout1.close();
+	fout2.close();
+
+	return 0;
+}
+
---
+++ Code/Runs/showresults.py
@@ -0,0 +1,102 @@
+from pylab import *
+import scipy
+from postprocess import logsumexp
+
+# Temperatures
+T1, T2 = 0.3, 0.3
+
+# First calculate things about the scalars (e.g. the normalising constant)
+scalars = loadtxt('scalars.txt')
+logw = loadtxt('logw.txt')
+smallest = min([scalars.shape[0], logw.size])
+scalars = scalars[0:smallest, :]
+logw = logw[0:smallest]
+
+# Prior weights, normalised
+logw = logw - logsumexp(logw)
+
+# Posterior weights, unnormalised
+logW = logw + scalars[:,0]/T1 + scalars[:,1]/T2
+
+# Normaliser
+logZ = logsumexp(logW)
+
+# Posterior weights, normalised
+logWW = logW - logZ
+ess = exp(-sum(exp(logWW)*logWW))
+
+# Information
+H = sum(exp(logWW)*(logWW - logw))
+
+print('log(Z) = {logZ}'.format(logZ=logZ))
+print('H = {H} nats'.format(H=H))
+
+plot(exp(logWW))
+ylabel('Weight wrt canonical distribution')
+title('ESS (for purposes of normalising constant calc) = {ess}'.format(ess=ess))
+
+show()
+
+
+
+# Calculate log(Z) and H for some canonical distributions
+T1 = exp(linspace(-1.204, 10., 101))
+T2 = exp(linspace(-1.204, 10., 101))
+[T1, T2] = meshgrid(T1, T2)
+T2 = T2[::-1, :]
+logZ = T1.copy()
+H = T1.copy()
+ess = T1.copy()
+
+for i in xrange(0, 101):
+  for j in xrange(0, 101):
+    # Prior weights, normalised
+    logw = logw - logsumexp(logw)
+
+    # Posterior weights, unnormalised
+    logW = logw + scalars[:,0]/T1[i, j] + scalars[:,1]/T2[i, j]
+
+    # Normaliser
+    logZ[i, j] = logsumexp(logW)
+
+    # Posterior weights, normalised
+    logWW = logW - logZ[i, j]
+    ess[i,j] = exp(-sum(exp(logWW)*logWW))
+
+    # Information
+    H[i, j] = sum(exp(logWW)*(logWW - logw))
+  print(i+1)
+
+logZ[ess < 5] = nan
+H[ess < 5] = nan
+
+figure(1)
+subplot(1, 2, 1)
+imshow(logZ, extent=(log(T1.min()), log(T1.max()), log(T2.min()), log(T2.max())))
+xlabel('ln($T_1$)')
+ylabel('ln($T_2$)')
+title('log(Z)')
+
+subplot(1, 2, 2)
+imshow(H, extent=(log(T1.min()), log(T1.max()), log(T2.min()), log(T2.max())))
+xlabel('ln($T_1$)')
+ylabel('ln($T_2$)')
+title('H, max={m}'.format(m=H[logical_not(isnan(H))].max()))
+
+figure(2)
+imshow(ess, extent=(log(T1.min()), log(T1.max()), log(T2.min()), log(T2.max())))
+xlabel('ln($T_1$)')
+ylabel('ln($T_2$)')
+title('ESS, max = {m}'.format(m=ess.max()))
+
+show()
+
+true_logZ = loadtxt('true_logZ.txt')
+true_H = loadtxt('true_H.txt')
+error = logZ - true_logZ
+lowest = error[logical_not(isnan(error))].min()
+highest = error[logical_not(isnan(error))].max()
+imshow(error/sqrt(true_H))
+title('{a}, {b}'.format(a=lowest, b=highest))
+show()
+
