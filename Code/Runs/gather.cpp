76b5355e64024e480042fda9384395a4deb28ca4
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-10-14 10:44:10 +1300

    Calculate ENR

--- Code/Runs/gather.cpp
+++ Code/Runs/gather.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <cmath>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -30,12 +31,13 @@ int main()
 	bool save = false;
 
 	vector<string> directories;
-	directories.push_back(string("10.1"));
-	directories.push_back(string("10.2"));
-	directories.push_back(string("10.3"));
-	directories.push_back(string("10.4"));
-	directories.push_back(string("10.5"));
-	directories.push_back(string("10.6"));
+	directories.push_back(string("mcmc_2000/8.1"));
+	directories.push_back(string("mcmc_2000/8.2"));
+	directories.push_back(string("mcmc_2000/8.3"));
+	directories.push_back(string("mcmc_2000/8.4"));
+	directories.push_back(string("mcmc_2000/8.5"));
+	directories.push_back(string("mcmc_2000/8.6"));
+	directories.push_back(string("mcmc_2000/8.7"));
 
 	fstream fout1, fout2;
 	if(save)
@@ -81,23 +83,59 @@ int main()
 		fout2.close();
 	}
 
-	// Calculate log(Z) and H
+	// Calculate log(Z)
 	double logZ = -1E300;
 	for(size_t i=0; i<logL.size(); i++)
 		logZ = logsumexp(logZ, (logw[i] - logC) + logL[i]);
 
+	// Calculate partial log(Z)s from each run
+	double logw_max = *max_element(logw.begin(), logw.end());
+	vector<double> logZ_partial, logC_partial;
+	for(size_t i=0; i<logL.size(); i++)
+	{
+		if(logw[i] == logw_max)
+		{
+			logZ_partial.push_back(-1E300);
+			logC_partial.push_back(-1E300);
+		}
+		logZ_partial[logZ_partial.size() - 1] = logsumexp(logZ_partial[logZ_partial.size() - 1], logw[i] + logL[i]);
+		logC_partial[logC_partial.size() - 1] = logsumexp(logC_partial[logC_partial.size() - 1], logw[i]);
+	}
+	// Normalise within each run
+	for(size_t i=0; i<logZ_partial.size(); i++)
+		logZ_partial[i] -= logC_partial[i];
+	double max = *max_element(logZ_partial.begin(), logZ_partial.end());
+	vector<double> Z(logZ_partial.size());
+	double tot = 0.;
+	for(size_t i=0; i<logZ_partial.size(); i++)
+	{
+		Z[i] = exp(logZ_partial[i] - max);
+		tot += Z[i];
+	}
+	for(size_t i=0; i<logZ_partial.size(); i++)
+		Z[i] /= tot;
+	// Effective number of runs
+	double ENR = 0.;
+	for(size_t i=0; i<Z.size(); i++)
+		ENR += -Z[i]*log(Z[i] + 1E-300);
+	ENR = exp(ENR);
+	cout<<"# Effective number of contributing runs = "<<ENR<<"."<<endl;
+
+	
+	// Calculate H
 	double H = 0.;
 	double logP;
 	for(size_t i=0; i<logL.size(); i++)
 	{
 		// Posterior weight
 		logP = logw[i] - logC + logL[i] - logZ;
-		H += exp(logP)*(logL[i] - logZ);
+		H += exp(logP)*logL[i];
 	}
+	H -= logZ;
 
-
+	// Output
 	cout<<setprecision(8);
-	cout<<"# ln(Z) = "<<logZ<<endl;
+	cout<<"# ln(Z) = "<<logZ<<"."<<endl;
 	cout<<"# H = "<<H<<" nats."<<endl;
 
 	return 0;
