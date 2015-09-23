76e4d0c200ee2c8ae9b64de8ad8d5bbd5cd18154
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-23 16:09:29 +0100

    Print the log likelihood

--- Code/C++/display_imageentropy.py
+++ Code/C++/display_imageentropy.py
@@ -9,6 +9,7 @@ for i in xrange(0, output.shape[0]):
 	x = output[i, 1:-1].reshape((100, 100))
 	imshow(x, interpolation='nearest', cmap='gray')
 	title(str(i+1) + '/' + str(output.shape[0]))
+	print(output[i, -1])
 	draw()
 ioff()
 show()
