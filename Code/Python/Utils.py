52f013756dac76f5d86727606de95f2169b25537
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-27 11:25:03 +0100

    logsumexp and logdiffexp

---
+++ Code/Python/Utils.py
@@ -0,0 +1,17 @@
+"""
+Various useful functions
+"""
+
+def logsumexp(values):
+	biggest = np.max(values)
+	x = values - biggest
+	result = np.log(np.sum(np.exp(x))) + biggest
+	return result
+
+def logdiffexp(x1, x2):
+	biggest = x1
+	xx1 = x1 - biggest
+	xx2 = x2 - biggest
+	result = np.log(np.exp(xx1) - np.exp(xx2)) + biggest
+	return result
+
