5bb6b10eb8e70faeca4465ca99931a8d588590ba
Brendon Brewer <brendon.brewer@gmail.com>
2015-09-30 12:11:08 -0400

    More functions

--- Code/Julia/Sampler.jl
+++ Code/Julia/Sampler.jl
@@ -18,10 +18,24 @@ type Sampler
 	forbidden_rectangles::Array{Float64, 2}
 end
 
-
 # A constructor that just takes the number of walkers
 function Sampler(num_walkers::Int64)
 	return Sampler(num_walkers, Array(Walker, num_walkers), 0.0, 0,
 				Array(Float64, (num_scalars, 0)))
+end
+
+# Generate all the walkers from the prior
+function initialise!(sampler::Sampler)
+	for(i in 1:sampler.num_walkers)
+		sampler.walkers[i] = Walker()
+		from_prior!(sampler.walkers[i])
+	end
+	return nothing
+end
+
+# Is 'scalars' inside the rectangle defined by 'rectangle'?
+function is_in_rectangle(scalars::Array{Float64, 1},
+							rectangle::Array{Float64, 1})
+	return all(scalars < rectangle)
 end
 
