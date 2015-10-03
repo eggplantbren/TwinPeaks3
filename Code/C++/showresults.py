9f66f81acc001dcd610c6de4b2e4c28870167697
Brendon Brewer <brendon.brewer@gmail.com>
2015-10-03 16:10:41 -0400

    New plot

--- Code/C++/showresults.py
+++ Code/C++/showresults.py
@@ -23,12 +23,20 @@ def H(temperatures):
 				- logZ(temperatures)
 	return np.sum(np.exp(logp)*(logp - output[:,0]))
 
-print('log(Z) = {a}'.format(a=logZ([0.1, 1.])))
-print('H = {h}'.format(h=H([0.1, 1.])))
+temperatures = [0.1, 1.]
+print('log(Z) = {a}'.format(a=logZ(temperatures)))
+print('H = {h}'.format(h=H(temperatures)))
 
 import matplotlib.pyplot as plt
+plt.figure(1)
 plt.plot(output[:,1], output[:,2], 'b.', markersize=1)
 plt.xlabel('Scalar 1')
 plt.ylabel('Scalar 2')
+
+plt.figure(2)
+logweight = output[:,0] + output[:,1]/temperatures[0] + output[:,2]/temperatures[1]
+plt.plot(np.exp(logweight - logweight.max()))
+plt.xlabel('Iteration')
+plt.ylabel('Relative posterior weight')
 plt.show()
 
