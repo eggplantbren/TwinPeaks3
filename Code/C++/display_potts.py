ab26bee33e15e9d0323f8d45ccb039c179bee642
Brendon Brewer <brendon.brewer@gmail.com>
2015-11-05 13:26:06 +1300

    xrange --> range

--- Code/C++/display_potts.py
+++ Code/C++/display_potts.py
@@ -5,7 +5,7 @@ output = atleast_2d(loadtxt('sample.txt'))
 
 ion()
 hold(False)
-for i in xrange(0, output.shape[0]):
+for i in range(0, output.shape[0]):
 	x = output[i, 3:].reshape((50, 50))
 	imshow(x, interpolation='nearest', cmap='jet')
 	title(str(i+1) + '/' + str(output.shape[0]))
