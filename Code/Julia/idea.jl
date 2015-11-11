099a1e09bf449cc6a63fbccc4aaa0f1b8fe2b499
Brendon Brewer <brendon.brewer@gmail.com>
2015-11-12 11:21:28 +1300

    Better algorithm

--- Code/Julia/idea.jl
+++ Code/Julia/idea.jl
@@ -3,33 +3,39 @@ using PyCall
 @pyimport colormaps
 
 # Number of pixels
-(ni, nj) = (21, 21)
-
-# Pixel widths
-(dx, dy) = (1.0/nj, 1.0/ni)
+(ni, nj) = (101, 101)
 
 # Prior distribution
 prior = Array(Float64, (ni, nj))
 for(j in 1:nj)
 	for(i in 1:ni)
-		prior[i, j] = exp(-0.5*(i - 0.5*ni)^2/2^2 - 0.5*(j - (ni - i))^2/2^2)
+		prior[i, j] = exp(-0.5*(j - 0.5*nj)^2/10^2
+							-0.5*(i - 0.5*ni)^2/10^2)
 	end
 end
 prior = prior/sum(prior)
 
 # Upper corner mass
 G = Array(Float64, (ni, nj))
-for(j in 1:nj)
+for(j in nj:-1:1)
 	for(i in 1:ni)
-		G[i, j] = 0.0
-		for(jj in j:nj)
-			for(ii in 1:i)
-				G[i, j] += prior[ii, jj]
-			end
+		up = 0.0
+		right = 0.0
+		up_and_right = 0.0
+		if(i != 1)
+			up += G[i-1, j]
 		end
+		if(j != nj)
+			right += G[i, j+1]
+		end
+		if((i != 1) && (j != nj))
+			up_and_right += G[i-1, j+1]
+		end
+
+		G[i, j] = prior[i, j] + up + right - up_and_right
 	end
 end
 
-plt.imshow((prior./G), cmap=colormaps.viridis, interpolation="nearest")
+plt.imshow(prior, cmap=colormaps.viridis, interpolation="nearest")
 plt.show()
 
