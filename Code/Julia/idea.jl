847b6b34dc9e0b13697eaf3434e091adfa7965d8
Brendon Brewer <brendon.brewer@gmail.com>
2015-11-12 13:02:40 +1300

    Use logs

--- Code/Julia/idea.jl
+++ Code/Julia/idea.jl
@@ -1,44 +1,46 @@
 using PyCall
 @pyimport matplotlib.pyplot as plt
 @pyimport colormaps
+include("Utils.jl")
 
 # Number of pixels
-(ni, nj) = (101, 101)
+(ni, nj) = (1024, 1024)
 
 # Prior distribution
-prior = Array(Float64, (ni, nj))
+log_prior = Array(Float64, (ni, nj))
 for(j in 1:nj)
+	x = (j - 0.5)/nj
 	for(i in 1:ni)
-		prior[i, j] = exp(-0.5*(j - 0.5*nj)^2/10^2
-							-0.5*(i - 0.5*ni)^2/10^2)
+		y = 1.0 - (i - 0.5)/ni
+		log_prior[i, j] = -0.5*(x - 0.1)^2/0.1^2 - 0.5*(y - x^2)^2/0.1^2
 	end
 end
-prior = prior/sum(prior)
+log_prior -= logsumexp(vec(log_prior))
 
 # Upper corner mass
-G = Array(Float64, (ni, nj))
+logG = Array(Float64, (ni, nj))
 for(j in nj:-1:1)
 	for(i in 1:ni)
-		up = 0.0
-		right = 0.0
-		up_and_right = 0.0
+		up = -Inf
+		right = -Inf
+		up_and_right = -Inf
 		if(i != 1)
-			up += G[i-1, j]
+			up = logG[i-1, j]
 		end
 		if(j != nj)
-			right += G[i, j+1]
+			right = logG[i, j+1]
 		end
 		if((i != 1) && (j != nj))
-			up_and_right += G[i-1, j+1]
+			up_and_right = logG[i-1, j+1]
 		end
-
-		G[i, j] = prior[i, j] + up + right - up_and_right
+		logG[i, j] = logsumexp([log_prior[i, j], up, right])
+		logG[i, j] = logdiffexp(logG[i, j], up_and_right)
 	end
 end
 
 plt.figure(figsize=(14, 6))
 plt.subplot(1, 2, 1)
-plt.imshow(prior, cmap=colormaps.viridis, interpolation="nearest")
+plt.imshow(exp(log_prior), cmap=colormaps.viridis, interpolation="nearest")
 plt.title("\$\\pi(L_1, L_2)\$", fontsize=16)
 plt.xlabel("\$L_1\$", fontsize=16)
 plt.ylabel("\$L_2\$", fontsize=16)
@@ -46,8 +48,8 @@ plt.gca()[:set_xticklabels]([])
 plt.gca()[:set_yticklabels]([])
 
 plt.subplot(1, 2, 2)
-plt.imshow(prior./G, cmap=colormaps.viridis, interpolation="nearest")
-plt.title("\$\\pi(L_1, L_2)/G(L_1, L_2)\$", fontsize=16)
+plt.imshow(log_prior - logG, cmap=colormaps.viridis, interpolation="nearest")
+plt.title("\$\\log\\left[\\pi(L_1, L_2)/G(L_1, L_2)\\right]\$", fontsize=16)
 plt.xlabel("\$L_1\$", fontsize=16)
 plt.ylabel("\$L_2\$", fontsize=16)
 plt.gca()[:set_xticklabels]([])
