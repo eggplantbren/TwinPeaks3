1042a34a5645886574becb0b98031574d7e070ef
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-11-13 19:45:17 +1300

    corner counts

--- Code/Julia/Sampler.jl
+++ Code/Julia/Sampler.jl
@@ -5,6 +5,7 @@ type Sampler
 	num_particles::Int64
 	particles::Vector{Particle}
 	all_scalars::Matrix{Float64}
+	uccs::Vector{Int64}
 end
 
 @doc """
@@ -12,7 +13,8 @@ A constructor. Input the number of particles.
 """ ->
 function Sampler(num_particles::Int64)
 	return Sampler(num_particles, Array(Particle, (num_particles, )),
-									Array(Float64, (num_particles, 2)))
+									Array(Float64, (num_particles, 2)),
+									Array(Int64, (num_particles, )))
 end
 
 @doc """
@@ -24,6 +26,20 @@ function initialise!(sampler::Sampler)
 		from_prior!(sampler.particles[i])
 		sampler.all_scalars[i, :] = calculate_scalars(sampler.particles[i])
 	end
+	calculate_uccs!(sampler::Sampler)
 end
 
+@doc """
+Calculate the upper corner count of each particle.
+""" ->
+function calculate_uccs!(sampler::Sampler)
+	for(i in 1:sampler.num_particles)
+		sampler.uccs[i] = 0
+		for(j in 1:sampler.num_particles)
+			if(all(sampler.all_scalars[j, :] .> sampler.all_scalars[i, :]))
+				sampler.uccs[i] += 1
+			end
+		end
+	end
+end
 
--- Code/Julia/main.jl
+++ Code/Julia/main.jl
@@ -1,6 +1,13 @@
+using PyCall
+@pyimport matplotlib.pyplot as plt
+
 include("models/Example.jl")
 include("Sampler.jl")
 
 sampler = Sampler(100)
 initialise!(sampler)
+
+plt.scatter(sampler.all_scalars[:,1], sampler.all_scalars[:,2], marker="o",
+						s=sampler.uccs)
+plt.show()
 
