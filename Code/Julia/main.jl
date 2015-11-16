18353e6f40a68a852b86b66d56a5351da377e9b8
Brendon Brewer <brendon.brewer@gmail.com>
2015-11-16 19:38:38 +1300

    A good way of excluding rectangles which approximates thresholding based on X(l1, l2).

--- Code/Julia/main.jl
+++ Code/Julia/main.jl
@@ -4,15 +4,28 @@ using PyCall
 include("models/Example.jl")
 include("Sampler.jl")
 
-sampler = Sampler(100)
+sampler = Sampler(1000)
 initialise!(sampler)
-worst = find_worst(sampler)
+indices = sortperm(sampler.uccs)
+reverse!(indices)
 
+plt.ion()
+plt.hold(true)
 plt.scatter(sampler.all_scalars[:,1], sampler.all_scalars[:,2], marker="o",
-						s=sampler.uccs)
-plt.scatter(sampler.all_scalars[worst,1], sampler.all_scalars[worst,2],
-					marker="o", color="r", s=100)
+						s=0.5*sampler.uccs, alpha=0.1)
 plt.axis("scaled")
 plt.axis([0, 1, 0, 1])
+
+med = median(sampler.uccs)
+for(i in 1:div(sampler.num_particles, 2))
+	ii = indices[i]
+
+	plt.fill_between([0.0, sampler.all_scalars[ii, 1]],
+						sampler.all_scalars[ii, 2], color=[0.5, 0.5, 0.5])
+	plt.title(string(i, " rectangles."))
+	plt.draw()
+end
+
+plt.ioff()
 plt.show()
 
