235978e932b96313bec03b91c0adc61002f81c88
Brendon Brewer <brendon.brewer@gmail.com>
2015-11-21 17:08:20 +1300

    Simplifying for sort by UCC method

--- Code/Julia/Sampler.jl
+++ Code/Julia/Sampler.jl
@@ -5,8 +5,7 @@ type Sampler
 	num_particles::Int64
 	particles::Vector{Particle}
 	all_scalars::Matrix{Float64}
-	uccs::Vector{Int64}
-	tiebreakers::Vector{Float64}
+	uccs::Vector{Float64}
 end
 
 @doc """
@@ -15,7 +14,6 @@ A constructor. Input the number of particles.
 function Sampler(num_particles::Int64)
 	return Sampler(num_particles, Array(Particle, (num_particles, )),
 									Array(Float64, (num_particles, 2)),
-									Array(Int64, (num_particles, )),
 									Array(Float64, (num_particles, )))
 end
 
@@ -27,7 +25,6 @@ function initialise!(sampler::Sampler)
 		sampler.particles[i] = Particle()
 		from_prior!(sampler.particles[i])
 		sampler.all_scalars[i, :] = calculate_scalars(sampler.particles[i])
-		sampler.tiebreakers[i] = rand()
 	end
 	calculate_uccs!(sampler::Sampler)
 end
@@ -37,43 +34,13 @@ Calculate the upper corner count of each particle.
 """ ->
 function calculate_uccs!(sampler::Sampler)
 	for(i in 1:sampler.num_particles)
-		sampler.uccs[i] = 0
+		sampler.uccs[i] = 0.5
 		for(j in 1:sampler.num_particles)
 			if(all(sampler.all_scalars[j, :] .> sampler.all_scalars[i, :]))
-				sampler.uccs[i] += 1
+				sampler.uccs[i] += 1.0
 			end
 		end
 	end
-end
-
-@doc """
-Find the particle with the highest (ucc, tiebreaker) combo.
-""" ->
-function find_worst(sampler::Sampler)
-	which = 1
-	for(i in 2:sampler.num_particles)
-		worse = false
-		if(sampler.uccs[i] > sampler.uccs[which])
-			worse = true
-		end
-		if(sampler.uccs[i] == sampler.uccs[which])
-			if(sampler.tiebreakers[i] > sampler.tiebreakers[which])
-				worse = true
-			end
-		end
-
-		if(worse)
-			which = i
-		end
-	end
-	return which
-end
-
-@doc """
-Do one iteration of Nested Sampling.
-""" ->
-function do_iteration!(sampler::Sampler)
-	worst = find_worst(sampler)
-	println(sampler.uccs[worst], " ", sampler.tiebreakers[worst])
+	return nothing
 end
 
--- Code/Julia/main.jl
+++ Code/Julia/main.jl
@@ -1,61 +1,33 @@
+# Setup plotting and include other files
 using PyCall
 @pyimport matplotlib.pyplot as plt
-
 include("models/Example.jl")
 include("Sampler.jl")
 
-sampler = Sampler(1000)
+# Use an odd number of particles
+num_particles = 101
+@assert mod(num_particles, 2) != 0
+
+# Create and initialise a Sampler
+sampler = Sampler(num_particles)
 initialise!(sampler)
+
+# argsort the uccs
 indices = sortperm(sampler.uccs)
 reverse!(indices)
 
-plt.ion()
-plt.hold(true)
+# Find the ucc threshold
+threshold_index = 1 + div(num_particles, 2)
+while(sampler.uccs[threshold_index+1] == sampler.uccs[threshold_index])
+	threshold_index += 1
+end
+
+println(threshold_index)
+#threshold = sampler.uccs[indices[]]
+
 plt.scatter(sampler.all_scalars[:,1], sampler.all_scalars[:,2], marker="o",
 						s=0.5*sampler.uccs, alpha=0.1)
 plt.axis("scaled")
 plt.axis([0, 1, 0, 1])
-
-med = median(sampler.uccs)
-rectangles = Array(Float64, (sampler.num_particles, 2))
-
-for(i in 1:div(sampler.num_particles, 2))
-	ii = indices[i]
-	rectangles[i, :] = sampler.all_scalars[ii, :]
-end
-rectangles = rectangles[1:div(sampler.num_particles, 2), :]
-
-for(i in 1:size(rectangles)[1])
-	plt.fill_between([0.0, rectangles[i, 1]],
-						rectangles[i, 2], color=[0.5, 0.5, 0.5])
-	plt.title(string(i, " rectangles."))
-	if(rem(i, 10) == 0)
-		plt.draw()
-	end
-end
-
-# Estimate the fraction of prior mass we just excluded
-reps = 100000
-num_ok = 0
-for(i in 1:reps)
-	particle = Particle()
-	from_prior!(particle)
-	scalars = calculate_scalars(particle)
-	is_okay = true
-	for(j in 1:size(rectangles)[1])
-		if(all(scalars .< vec(rectangles[j, :])))
-			is_okay = false
-		end
-	end
-	if(is_okay)
-		num_ok += 1
-		plt.plot(calculate_scalars(particle)[1], calculate_scalars(particle)[2], "r.", markersize=3)
-	end
-	if(rem(i, 100) == 0)
-		plt.draw()
-	end
-	println(i, " ", num_ok)
-end
-
-plt.ioff()
 plt.show()
+
