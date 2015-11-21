0a64bbf57042828d28c8ae0062757cdf93d5aa4a
Brendon Brewer <brendon.brewer@gmail.com>
2015-11-21 17:30:16 +1300

    Fixed error

--- Code/Julia/main.jl
+++ Code/Julia/main.jl
@@ -14,20 +14,17 @@ initialise!(sampler)
 
 # argsort the uccs
 indices = sortperm(sampler.uccs)
-reverse!(indices)
 
 # Find the ucc threshold
 threshold_index = 1 + div(num_particles, 2)
 while((threshold_index != num_particles)
-		&& (sampler.uccs[threshold_index+1] == sampler.uccs[threshold_index]))
+		&& (sampler.uccs[indices[threshold_index+1]] == sampler.uccs[indices[threshold_index]]))
 	threshold_index += 1
 end
-
-println(threshold_index)
-#threshold = sampler.uccs[indices[]]
+threshold = sampler.uccs[indices[threshold_index]]
 
 plt.scatter(sampler.all_scalars[:,1], sampler.all_scalars[:,2], marker="o",
-						s=0.5*sampler.uccs, alpha=0.1)
+						s=0.5*sampler.uccs, alpha=0.5)
 plt.axis("scaled")
 plt.axis([0, 1, 0, 1])
 plt.show()
