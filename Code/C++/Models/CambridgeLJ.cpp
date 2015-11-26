62cd57d2714cc206f74b5f12945c7dc300285caa
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-11-26 20:01:11 +1300

    declare fortran function

--- Code/C++/Models/CambridgeLJ.cpp
+++ Code/C++/Models/CambridgeLJ.cpp
@@ -6,6 +6,16 @@
 using namespace std;
 using namespace DNest3;
 
+// To call Rob's Fortran functions
+extern "C"
+{
+	void __twin_peaks_routines_MOD_initialise_config(double* s, double* h0,
+					double* V, double* ener, double* min_height,
+					double* Vmax, int* N, double* cutoff,
+					double* univar_3Nplus10001, double* univar_60k,
+					double* univar_30k, double* nvar_60k, bool* flat_v_prior);
+}
+
 CambridgeLJ::CambridgeLJ()
 :scalars(2)
 {
