6986c325ce36205514543d333a273e20897af5e3
Brendon J. Brewer <brendon.brewer@gmail.com>
2015-11-30 16:11:28 +1300

    display script for SimpleExample

---
+++ Code/C++/display_simpleexample.py
@@ -0,0 +1,17 @@
+from pylab import *
+
+output = atleast_2d(loadtxt('sample.txt'))
+
+figure(figsize=(13, 6))
+ion()
+hold(False)
+for i in xrange(0, output.shape[0]):
+	model = output[i, 3:]
+	hist(model, 300, alpha=0.2)
+	axis([0., 1., 0., 10.])
+	title(i+1)
+	draw()
+
+ioff()
+show()
+
