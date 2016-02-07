b949679d66899417b8dc5d2aaf214176d8781d04
Brendon Brewer <brendon.brewer@gmail.com>
2016-02-07 16:15:05 +1300

    Getting there

--- Paper/figures/sweep.py
+++ Paper/figures/sweep.py
@@ -9,9 +9,45 @@ const decreases.
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Let x=X1, y=X2|X1
+def deriv(state):
+	return (state*np.log(state) - state)
 
-dt = 0.01
+# Let x=X1, y=X2|X1
+x = np.linspace(1E-6, 1, 1001)
+
+# Timestep
+t_final, dt = 10., 0.005
+steps = int(t_final/dt)
+
+# Initial condition for threshold
+C = 1.
+
+# Set up plotting
+plt.figure(figsize=(8, 8))
+plt.ion()
+plt.hold(False)
 
 for i in range(0, steps):
+	f1 = deriv(C)
+	f2 = deriv(C + 0.5*dt*f1)
+	f3 = deriv(C + 0.5*dt*f2)
+	f4 = deriv(C + dt*f3)
+	C += dt/6*(f1 + 2*f2 + 2*f3 + f4)
+
+	y = C/x
+	plt.plot(x, y, 'k-', linewidth=2)
+	plt.xlabel('$x$', fontsize=18)
+	plt.ylabel('$y$', fontsize=18)
+	plt.axis([0, 1, 0, 1])
+	plt.title('$X = {X:.4f}$'.format(X=C-C*np.log(C)), fontsize=16)
+
+	yy = y.copy()
+	yy[y > 1.] = 1.
+	plt.fill_between(x, yy, color='cyan', alpha=0.3)
+	print(f1/C)
+	plt.draw()
+
+# Finalise plotting
+plt.ioff()
+plt.show()
 
