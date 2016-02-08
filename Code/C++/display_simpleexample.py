92cd30b102899c9b70e6b6284a67cea901303b41
Brendon Brewer <brendon.brewer@gmail.com>
2016-02-08 19:16:32 +1300

    xrange --> range

--- Code/C++/display_simpleexample.py
+++ Code/C++/display_simpleexample.py
@@ -5,7 +5,7 @@ output = atleast_2d(loadtxt('sample.txt'))
 figure(figsize=(13, 6))
 ion()
 hold(False)
-for i in xrange(0, output.shape[0]):
+for i in range(0, output.shape[0]):
 	model = output[i, 3:]
 	hist(model, 300, alpha=0.2)
 	axis([0., 1., 0., 10.])
