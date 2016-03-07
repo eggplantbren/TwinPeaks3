56b2987885c272421ecc1341158dfc0912a20d11
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-03-08 11:56:42 +1300

    New results

--- Paper/figures/error.py
+++ Paper/figures/error.py
@@ -11,18 +11,18 @@ results = loadtxt("../../Code/C++/results.dat")
 which = (results[:,1] == 500)
 loglog(results[which, 0], results[which, 2], "bo-", label="500 MCMC steps")
 
-# Plot second set of results
-which = (results[:,1] == 1000)
-loglog(results[which, 0], results[which, 2], "go-", label="1000 MCMC steps")
+## Plot second set of results
+#which = (results[:,1] == 1000)
+#loglog(results[which, 0], results[which, 2], "go-", label="1000 MCMC steps")
 
-# Plot third set of results
-which = (results[:,1] == 2000)
-loglog(results[which, 0], results[which, 2], "yo-", label="2000 MCMC steps")
+## Plot third set of results
+#which = (results[:,1] == 2000)
+#loglog(results[which, 0], results[which, 2], "yo-", label="2000 MCMC steps")
 
 # Axis labels and legend
 xlabel("Number of particles, $N$")
 ylabel("RMS error in $\\ln Z(\\beta_1, \\beta_2)$")
-legend(loc="upper right")
+#legend(loc="upper right")
 
 # Save and display the figure
 savefig("error.pdf", bbox_inches="tight")
