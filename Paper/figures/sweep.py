3b3204402a34c8ecc403a9886809f6443870117a
Brendon Brewer <brendon.brewer@gmail.com>
2016-03-10 19:07:57 +1300

    Removed interactive mode

--- Paper/figures/sweep.py
+++ Paper/figures/sweep.py
@@ -8,9 +8,12 @@ const decreases.
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+os.system("rm Frames/*.png")
 
 def deriv(state):
-	return (state - state*np.log(state))/np.log(state)
+    return (state - state*np.log(state))/np.log(state)
 
 # Let x=X1, y=X2|X1
 x = np.linspace(1E-6, 1, 1001)
@@ -24,34 +27,30 @@ C = 0.99
 
 # Set up plotting
 plt.figure(figsize=(8, 8))
-plt.ion()
 plt.hold(False)
 
 for i in range(0, steps):
-	f1 = deriv(C)
-	f2 = deriv(C + 0.5*dt*f1)
-	f3 = deriv(C + 0.5*dt*f2)
-	f4 = deriv(C + dt*f3)
-	C += dt/6*(f1 + 2*f2 + 2*f3 + f4)
+    f1 = deriv(C)
+    f2 = deriv(C + 0.5*dt*f1)
+    f3 = deriv(C + 0.5*dt*f2)
+    f4 = deriv(C + dt*f3)
+    C += dt/6*(f1 + 2*f2 + 2*f3 + f4)
 
-	y = C/x
-	X = C - C*np.log(C)
-	plt.plot(x, y, 'k-', linewidth=2)
-	plt.xlabel(r'$X(L_1)$', fontsize=18)
-	plt.ylabel(r'$X(L_2 | L_1)$', fontsize=18)
-	plt.axis([0, 1, 0, 1])
-	plt.title('$X = {X:.4f}$'.format(X=X), fontsize=18)
+    y = C/x
+    X = C - C*np.log(C)
+    plt.plot(x, y, 'k-', linewidth=2)
+    plt.xlabel(r'$X(L_1)$', fontsize=18)
+    plt.ylabel(r'$X(L_2 | L_1)$', fontsize=18)
+    plt.axis([0, 1, 0, 1])
+    plt.title('$X = {X:.4f}$'.format(X=X), fontsize=18)
 
-	yy = y.copy()
-	yy[y > 1.] = 1.
-	plt.fill_between(x, yy, color='cyan', alpha=0.1)
+    yy = y.copy()
+    yy[y > 1.] = 1.
+    plt.fill_between(x, yy, color='cyan', alpha=0.1)
 
-	plt.axhline(X, linestyle='--', color='g')
-	plt.axvline(X, linestyle='--', color='r')
-	plt.savefig('%0.4d' % (i + 1) + '.png', bbox_inches='tight')
-	plt.draw()
+    plt.axhline(X, linestyle='--', color='g')
+    plt.axvline(X, linestyle='--', color='r')
+    plt.savefig('Frames/%0.6d' % (i + 1) + '.png', bbox_inches='tight')
+    print("Saved frame {k}/{N}".format(k=(i+1), N=steps))
 
-# Finalise plotting
-plt.ioff()
-plt.show()
 
